d46ecc538c87d633910fc3c862fd9d32bc33c927
Avi-1996 <36784277+Avi-1996@users.noreply.github.com>
2018-07-05 21:12:24 +0100

    Create CompoundInterst.py

---
+++ 100DayCode/Day5/CompoundInterst.py
@@ -0,0 +1,6 @@
+from math import pow
+p=int(input("Enter the Amount"))#taking the pricipal amount
+r=float(input("Enter the rate"))#taking the rate of interest
+t=int(input("Enter the year"))  #taking the time in for of year
+a=p*(pow((1+r/100),t)-1)        #applying the formula
+print("compound iNTEREST iS",a) #printing the result
