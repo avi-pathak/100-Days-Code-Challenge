3eec23c4d41c63b09753f5943e91abdaf76bb84d
Avi-1996 <36784277+Avi-1996@users.noreply.github.com>
2018-07-05 15:06:19 +0100

    Update PhoneNumneber.py

--- 100DayCode/Day4/PhoneNumneber.py
+++ 100DayCode/Day4/PhoneNumneber.py
@@ -1,25 +1,14 @@
-l=['91-896463685','91-66645645','+1-556566','+52-563324563']
-num,di='',{}
-for x in l:
-     for i in x:
-          if i=='-':
-               break
-          num+=i
-     if num in di.keys():
-          di[num]+=" "+x[3:]
-     else:
-          di[num]=x[3:]
-     num=''
-print(di)
-li=list(di.values())
-for k,v in di.items():
-     print(k,"----->",v)
-list,m=[],''
-for k,v in di.items():
-     for i in v.split():
-          m+=k+i+","
-     list.append(m)
-     m=''
-     print(li)
-for i in list:
-     print("NUMB ",l)
+l=['+91-896463685','+91-66645645','+12-556566','+12-996654533','+52-563324563','+82-665533265']
+l2=['+91','+12','+52','+82']
+li=[[''] for x in range(len(l2))]
+i,k=0,1
+for y in l2:
+     for x in l:
+          if x[:3]==y:
+               li[i][0]+=x+","
+     i+=1
+for x in range(len(li)):
+     for y in range(len(li[x])):
+          print("NUMB",k,li[x][y])
+          k+=1
+     print()
