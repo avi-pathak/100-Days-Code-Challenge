775b7c0da05980ca130adbc1c0c40750c09c89ec
Avi-1996 <36784277+Avi-1996@users.noreply.github.com>
2018-07-07 06:29:52 +0100

    Update BinArray.py

--- 100DayCode/Day6/BinArray.py
+++ 100DayCode/Day6/BinArray.py
@@ -1,8 +1,8 @@
-from time import clock as c
-binarr=[int(x) for x in input("enter Numbers Seperated By space").split()]
+from time import clock as c #it only for calculating the excution time
+binarr=[int(x) for x in input("enter Numbers Seperated By space").split()] #taking Input
 t1=c()
-l2=[0 for x in range(binarr.count(0))]
-for x in range(binarr.count(1)):
+l2=[0 for x in range(binarr.count(0))] #at start of result push all no of Zero present the above list
+for x in range(binarr.count(1)):     #appending all no of One present in the above list
     l2.append(1)
 t2=c()
-print(l2,t2-t1)
+print(l2,t2-t1) #printing result and the excution time
