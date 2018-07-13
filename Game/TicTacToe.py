7329c8a61a470fe0408644e5204baf70e73eb446
Avi-1996 <36784277+Avi-1996@users.noreply.github.com>
2018-07-13 11:50:51 +0100

    Update TicTacToe.py

--- Game/TicTacToe.py
+++ Game/TicTacToe.py
@@ -7,12 +7,12 @@ def show():
      print(list[3],'|',list[4],'|',list[5],'|')
      print(list[6],'|',list[7],'|',list[8],'|')
 def isempty(n):
-     if(list[n]!='X' and list[n]!='O'):
+     if(list[n]!='#' and list[n]!='*'):
           return True
      else:
           return False
-def allEmpty():
-     return (all(x=='X' or x=='O' for x in list))
+def allFull():
+     return (all(x=='#' or x=='*' for x in list))
 def whoWins(char):
      if list[0]==char and list[1]==char and list[2]==char:
           return True
@@ -22,7 +22,7 @@ def whoWins(char):
           return True
      elif list[0]==char and list[3]==char and list[6]==char:
           return True
-     elif list[1]==char and list[4]==char and list[4]==char:
+     elif list[1]==char and list[4]==char and list[7]==char:
           return True
      elif list[2]==char and list[5]==char and list[8]==char:
           return True
@@ -32,23 +32,23 @@ def whoWins(char):
           return True
 show()
 while True:
-     if allEmpty():
+     if allFull():
           print("Match Draw")
           break
-     if whoWins('X'):
+     if whoWins('#'):
           print('YOU WIN')
           break;
-     if whoWins('O'):
+     if whoWins('*'):
           print('Computer Wins')
           break;
      key=int(input("enter the postion wheree you want to start:  "))
      if isempty(key):
-          list[key]='X'
+          list[key]='#'
      else:
           print("the postion is already taken tru anather One ")
      while True:
           comp=randint(0,8)
           if isempty(comp):
-               list[comp]='O'
+               list[comp]='*'
                show()
                break
