aedf22d0dfd3d8000dea580eaaeeb40506516ff1
Avi-1996 <36784277+Avi-1996@users.noreply.github.com>
2018-07-14 19:32:13 +0100

    Update MaxHeapToMeanHeap.py

--- 100DayCode/Day13/MaxHeapToMeanHeap.py
+++ 100DayCode/Day13/MaxHeapToMeanHeap.py
@@ -3,3 +3,26 @@ in to min HEAP'''
 l=[200,100,50,90,80,40,30,60,20,10]
 l.sort()
 print(l)
+
+#Second Method just Heapyfy in the rverse order
+#complexity is o(n)
+def heapyfy(l):
+     l2=[0 for x in range(len(l)+1)]
+     for i in range(1,len(l)):
+          if i==1:
+               l2[i]=l[i]
+          else:
+               l2[i]=l[i]
+               par=i//2
+               child=i
+               while par>=1:
+                    if l2[child] > l2[par]:
+                         break
+                    else:
+                         l2[par],l2[child]=l2[child],l2[par]
+                    child=par
+                    par//=2
+     l[:]=l2[1:]
+l=[None,200,100,50,90,80,40,30,60,20,10]
+heapyfy(l)
+print(l)
