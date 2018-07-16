169761b4fb1df9d7795effd815aca89add54efb6
Avi-1996 <36784277+Avi-1996@users.noreply.github.com>
2018-07-16 18:10:38 +0100

    Create pattern13.py

---
+++ 100DayCode/Day15/pattern13.py
@@ -0,0 +1,19 @@
+Pattern-13:
+--------------
+A
+B B
+C C C
+D D D D
+E E E E E
+F F F F F F
+G G G G G G G
+H H H H H H H H
+I I I I I I I I I
+J J J J J J J J J J
+
+
+n=int(input("Enter the number of rows: "))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(chr(64+i),end=" ")
+    print() 
