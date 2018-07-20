26af72e730bec5aa5ae148f937d12ae988ee0996
Avi-1996 <36784277+Avi-1996@users.noreply.github.com>
2018-07-20 23:23:52 +0530

    Update pattern7.py

--- 100DayCode/Day15/pattern7.py
+++ 100DayCode/Day15/pattern7.py
@@ -1,5 +1,5 @@
 
-Pattern-7
+'''Pattern-7
 -------------:
 10 9 8 7 6 5 4 3 2 1
 10 9 8 7 6 5 4 3 2 1
@@ -12,8 +12,9 @@ Pattern-7
 10 9 8 7 6 5 4 3 2 1
 10 9 8 7 6 5 4 3 2 1
 ----------------------------
+'''
 n=int(input("Enter the number of rows: "))
-for i in range(1,n+1):
-  for j in range(1,n+1):
-    print(n+1-j,end=" ")
-   print()
+for i in range(1,n+1):#each row
+  for j in range(1,n+1):#each column
+    print(n+1-j,end=" ")#print the elemen
+   print()#it is for going to the new line
