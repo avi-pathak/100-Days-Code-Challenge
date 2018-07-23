20c1d9f5e31e7708068b419138df5d1c79c7661a
Avi-1996 <36784277+Avi-1996@users.noreply.github.com>
2018-07-23 17:11:40 +0100

    Create Duplicate.py

---
+++ 100DayCode/Day21/Duplicate.py
@@ -0,0 +1,5 @@
+def duplicate(list):
+    return(list(set(list)))
+    
+    '''in this approch the given list first converted in to set and as set property says it will remove all the duplicate
+    and we want to return list thats agan we convert set to a list'''
