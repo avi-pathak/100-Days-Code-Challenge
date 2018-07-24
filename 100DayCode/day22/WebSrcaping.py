0bf4fb8d0d75c37cb8ab5b7910827cbd5fe674ae
Avi-1996 <36784277+Avi-1996@users.noreply.github.com>
2018-07-24 15:32:37 +0100

    Create WebSrcaping.py

---
+++ 100DayCode/day22/WebSrcaping.py
@@ -0,0 +1,26 @@
+i'''
+Alogo is:
+Use the requests library to load the HTML of the page into Python
+
+Set up BeautifulSoup to process the HTML
+
+Find out which HTML tags contain all the titles
+
+Use BeautifulSoup to extract all the titles from the HTML
+
+Format them nicely
+'''
+
+
+import requests
+from bs4 import BeautifulSoup
+ 
+base_url = 'http://www.nytimes.com'
+r = requests.get(base_url)
+soup = BeautifulSoup(r.text)
+ 
+for story_heading in soup.find_all(class_="story-heading"): 
+    if story_heading.a: 
+        print(story_heading.a.text.replace("\n", " ").strip())
+    else: 
+        print(story_heading.contents[0].strip())
