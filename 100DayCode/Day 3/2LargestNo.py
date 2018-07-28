45982bdc51d2a368ade99d7bfb5c01e4a309da80
Avi-1996 <36784277+Avi-1996@users.noreply.github.com>
2018-07-29 03:39:01 +0530

    Update 2LargestNo.py

--- 100DayCode/Day 3/2LargestNo.py
+++ 100DayCode/Day 3/2LargestNo.py
@@ -11,7 +11,7 @@ for x in l[2:]:
      counter+=1          #tell how many time loop will be excute
      if x>max[0]:        #give True if x is greater than max list
           temp=max[0]    #this line manupulate max list and make the largest at
-                         #second largest postion and x in lagest postion""" 
+                         #second largest postion and x in lagest postion"""      
           max[0]=x
           max[1]=temp
 print("first largest : {}\nSecond largest : {}".format(max[0],max[1]))
