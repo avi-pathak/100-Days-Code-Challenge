106fc2926934a670e059d18c22fa9300f6255b71
Avinash <36784277+Avi-1996@users.noreply.github.com>
2018-08-19 16:50:05 +0100

    Create Ques2.py

---
+++ 100DayCode/Day49/Ques2.py
@@ -0,0 +1,4 @@
+'' In this Question Just need to crack the formula for this pattern it took me 4 hours to break it plz try urself''
+
+for i in range(1,int(input())): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print(int(i*pow(10,i)/9))
