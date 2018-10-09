add8c0c311f9a087276807559c1ec45139d72763
Avinash <36784277+Avi-1996@users.noreply.github.com>
2018-10-09 22:11:58 +0530

    Create Ques1.py

---
+++ 100DayCode/Day98/Ques1.py
@@ -0,0 +1,22 @@
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from urllib.error import URLError
+
+try:
+    html = urlopen("https://abcxyz.com")
+except HTTPError as e:
+    print("HTTP error")
+except URLError as e:
+    print("Server not found!")
+else:
+    print(html.read())
+    
+try:
+    html = urlopen("http://www.example.com/")
+except HTTPError as e:
+    print("HTTP error")
+except URLError as e:
+    print("Server not found!")
+else:
+    print("HTML Details")    
+    print(html.read())  
